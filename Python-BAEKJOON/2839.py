fd7358829a48734f5ab3fec0954ca6adcb86f1b2
byhhh2 <rlaalsdl111@gmail.com>
2021-10-11 23:52:09 +0900

    Add step8

--- Python-BAEKJOON/2839.py
+++ Python-BAEKJOON/2839.py
@@ -3,19 +3,13 @@ import sys
 N = int(sys.stdin.readline())
 
 cnt = 0
+tmp = N
 
-if N % 5 == 0:
-    print(N // 5)
-elif (N % 5) % 3 == 0:
-    print((N // 5) + ((N % 5) // 3))
-else:
-    if (N % 5) % 3 != 0:
-        tmp = N
-        while (tmp > 0):
-            cnt += 1
-            tmp -= 3
-            if tmp % 5 == 0:
-                print(cnt + (tmp // 5))
-                exit(0)
-        print(-1)
+while (tmp >= 0):
+    if tmp % 5 == 0:
+        print(cnt + (tmp // 5))
+        exit(0)
+    cnt += 1
+    tmp -= 3
+print(-1)
 

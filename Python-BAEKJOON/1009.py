27285bed4772cd4883f0d8c906b15cc75ed2cf20
byhhh2 <rlaalsdl111@gmail.com>
2021-11-05 18:58:15 +0900

    Add 1009

--- Python-BAEKJOON/1009.py
+++ Python-BAEKJOON/1009.py
@@ -3,5 +3,16 @@ import sys
 T = int(sys.stdin.readline())
 
 for t in range(T):
+    baseList = []
+
     a, b = map(int, sys.stdin.readline().split())
-    print((a ** b) % 10)
+
+    base = a % 10
+    if base == 0:
+        print(10)
+        continue
+    
+    for i in range(1, 5):
+        baseList.append((base ** i) % 10)
+
+    print(baseList[(b % 4) + -1])

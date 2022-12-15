6b3feb8aec437a7f643fd0cf5284c962159d715b
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-12-15 18:11:36 +0900

    Add 120847

---
+++ JS-Programmers/최댓값 만들기(1).ts
@@ -0,0 +1,15 @@
+function solution(numbers: number[]) {
+  let max = numbers[0] * numbers[1];
+
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] * numbers[j] > max) {
+        max = numbers[i] * numbers[j];
+      }
+    }
+  }
+
+  return max;
+}
+
+export {};

b140acf7e041ba672c90f6271976d91f33bed9e6
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-12-22 23:06:52 +0900

    Add 120838

---
+++ JS-Programmers/모스부호 (1).ts
@@ -0,0 +1,44 @@
+// @ts-nocheck
+
+const morse = {
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+};
+
+function solution(letter: string) {
+  const spell = letter.split(' ');
+
+  for (const index in spell) {
+    spell[index] = morse[spell[index]];
+  }
+
+  return spell.join('');
+}
+
+console.log(solution('.... . .-.. .-.. ---'));
+
+export {};
